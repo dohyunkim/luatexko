b40dea2dcb1165fb8b23f3ccb5d7eb69faf59b65
Dohyun Kim <nomosnomos@gmail.com>
2021-02-19 11:04:04 +0900

    tuning previous commit: use font_options.monospaced

--- luatexko.lua
+++ luatexko.lua
@@ -472,17 +472,18 @@ end
 
 local function hangul_space_skip (curr, newfont)
   if curr.lang ~= nohyphen and curr.font ~= newfont then
-    local params = getparameters(curr.font)
     -- fontloader's "node" mode sets space_stretch to zero
     -- when the font is a monospaced font (fontspec's \setmonofont
     -- command does the same thing), which we will bypass here
     -- for alignment of CJK and Latin glyphs in verbatim environment.
     -- See http://www.ktug.org/xe/index.php?document_srl=249772
-    if params and params.space_stretch ~= 0 then
+    if not font_options.monospaced[curr.font] then
       local n = getnext(curr)
       if n and n.id == glueid and n.subtype == spaceskip then
+        local params = getparameters(curr.font)
         local oldwd, oldst, oldsh, oldsto, oldsho = getglue(n)
-        if oldwd == params.space
+        if params
+          and oldwd == params.space
           and oldst == params.space_stretch
           and oldsh == params.space_shrink
           and oldsto == 0
